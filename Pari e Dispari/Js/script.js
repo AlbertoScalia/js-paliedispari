14c03e1f52dac3d12583e95ea056f2f1c7d83002
AlbertoScalia <albscalia@gmail.com>
2023-04-21 15:49:56 +0200

    Feat (JS) : Add do-while cycle

---
+++ Pari e Dispari/Js/script.js
@@ -0,0 +1,8 @@
+//Scrivo la costante vuota
+let sceltaUtente = "";
+
+//Creo un primo ciclo do-while tale che la domanda venga posta almeno una volta
+do {
+    sceltaUtente = prompt("Scegli \"pari\" oppure \"dispari\"?");
+} while (sceltaUtente !== "pari" && sceltaUtente !== "dispari");
+
