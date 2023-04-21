3ae40c99ffca62fb9ad4b957c5ef9609b0d7508e
AlbertoScalia <albscalia@gmail.com>
2023-04-21 15:58:15 +0200

    Feat (JS) : Add do-while cycle and random num

--- Pari e Dispari/Js/script.js
+++ Pari e Dispari/Js/script.js
@@ -1,8 +1,20 @@
-//Scrivo la costante vuota
+//Scrivo le costanti
 let sceltaUtente = "";
+let numeroUtente = 0;
+let numeroRandom = 0;
+
 
 //Creo un primo ciclo do-while tale che la domanda venga posta almeno una volta
 do {
     sceltaUtente = prompt("Scegli \"pari\" oppure \"dispari\"?");
 } while (sceltaUtente !== "pari" && sceltaUtente !== "dispari");
+
+//Creo un secondo ciclo in cui faccio scegliere un numero da 1 a 5 
+do {
+    numeroUtente = prompt("Scegli un numero da 1 a 5");
+    numeroUtente = parseInt(numeroUtente);
+} while ((numeroUtente < 1 || numeroUtente > 5) || isNaN(numeroUtente));
+
+//Genero un numero randomico da 1 a 5
+numeroRandom = randomGenerator1_5();
 
